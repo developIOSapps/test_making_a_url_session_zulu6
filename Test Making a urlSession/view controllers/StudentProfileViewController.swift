11621ee2aedaec4fd4f525b5d21135350d04758b
Steven Hertz <hertzsteven@icloud.com>
2019-11-17 20:50:32 -0500

    After some cleanup

--- Test Making a urlSession/view controllers/StudentProfileViewController.swift
+++ Test Making a urlSession/view controllers/StudentProfileViewController.swift
@@ -22,23 +22,19 @@ class StudentProfileViewController: UIViewController {
     
     @IBOutlet weak var dayOfWeekSegment: UISegmentedControl!
     
+
     var notesDelegate: NotesDelegate?
     
-    
     var setAlready: Bool = false
-    
     
     var profiles: [Profile] = []
     
     var profileForTheDayArray = Array(repeating: String(), count: 5)
-    // var daySelectedRowIndexPathArray = Array(repeating: IndexPath(), count: 5)
     
     var segmentMovingFrom = 0
     
     var indexPathDictionary: [String: IndexPath] = [:]
-    
-
-    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,10 +47,7 @@ class StudentProfileViewController: UIViewController {
         LastName.text = student.lastName
         
         dayOfWeekSegment.selectedSegmentIndex = 0
-
         
-
-        // Do any additional setup after loading the view.
         
         GetDataApi.getProfileListResponse { (xyz) in
             DispatchQueue.main.async {
@@ -64,29 +57,19 @@ class StudentProfileViewController: UIViewController {
                 
                 self.profiles = profilesResponse.profiles.filter{$0.name.hasPrefix("Profile-App ") }
                 
-                
                 self.profiles.forEach { print($0.name ) }
                 
                 self.profilesTableView.reloadData()
                 
                 self.processStudentNotes()
                 
-//                for (idx, profile) in self.profileForTheDayArray.enumerated() {
-//                    self.daySelectedRowIndexPathArray[idx] = self.indexPathDictionary[profile] ??  IndexPath()
-//                }
-                
                 if let rownumber = self.profiles.firstIndex(where: { (item) -> Bool in
                     item.name == self.profileForTheDayArray[self.dayOfWeekSegment.selectedSegmentIndex]
                 }) {
                     self.profilesTableView.selectRow(at: IndexPath(row: rownumber, section: 0), animated: true, scrollPosition: .none)
+                    let profileSelected = self.profiles[rownumber]
+                    self.profileDescription.text = profileSelected.description
                 }
-
-//                let rownumber = self.profiles.firstIndex { (item) -> Bool in
-//                    item.name == self.profileForTheDayArray[0]
-//                }
-//                self.profilesTableView.selectRow(at: IndexPath(row: rownumber!, section: 0), animated: true, scrollPosition: .none)
-
-                
             }
         }
     }
@@ -109,14 +92,14 @@ class StudentProfileViewController: UIViewController {
             print("need to select a profile")
             return
         }
-        //let profileName = profiles[rowSelected].name
+
         GetDataApi.updateUserProperty(GeneratedReq.init(request: ValidReqs.updateUserProperty(userId: String(student.id), propertyName: "notes", propertyValue: studentProfileListComplete))) {
             DispatchQueue.main.async {
                 self.notesDelegate?.updateStudentNote(passedNoted: studentProfileListComplete)
                 print("````````````Hooray Job well done")
             }
         }
-
+        
     }
     
     @IBAction func dayOfWeekChanged(_ sender: UISegmentedControl) {
@@ -130,48 +113,35 @@ class StudentProfileViewController: UIViewController {
             else { sender.selectedSegmentIndex  = segmentMovingFrom
                 return
         }
-
-
         
-        /// save the selection for current selection
-//        daySelectedRowIndexPathArray[segmentMovingFrom] = selectionIndexPath
-//
-//        let row = selectionIndexPath.row
-//        profileForTheDayArray[segmentMovingFrom] = profiles[row].name
-
-            
         /// deselect current selection
         self.profilesTableView.deselectRow(at: selectionIndexPath, animated: true)
         /// save newSegment
         segmentMovingFrom = sender.selectedSegmentIndex
-            
+        
         /// set profile selected for current day
-//            if !daySelectedRowIndexPathArray[sender.selectedSegmentIndex].isEmpty {
-//                profilesTableView.selectRow(at: daySelectedRowIndexPathArray[sender.selectedSegmentIndex], animated: true, scrollPosition: .none)
-//            }
-//            else {
-                /// get the row number from the array of values
-                /// get the profile of the day
-                
-                if let rownumber = profiles.firstIndex(where: { (item) -> Bool in
-                    print("Check to hightlight row with --- \(profileForTheDayArray[sender.selectedSegmentIndex])")
-                    return item.name == profileForTheDayArray[sender.selectedSegmentIndex]
-                }) {
-                    profilesTableView.selectRow(at: IndexPath(row: rownumber, section: 0), animated: true, scrollPosition: .none)
-                    let profileSelected = profiles[rownumber]
-                    profileDescription.text = profileSelected.description
-                }
+        /// get the row number from the array of values
+        /// get the profile of the day
+        
+        if let rownumber = profiles.firstIndex(where: { (item) -> Bool in
+            print("Check to hightlight row with --- \(profileForTheDayArray[sender.selectedSegmentIndex])")
+            return item.name == profileForTheDayArray[sender.selectedSegmentIndex]
+        }) {
+            profilesTableView.selectRow(at: IndexPath(row: rownumber, section: 0), animated: true, scrollPosition: .none)
+            let profileSelected = profiles[rownumber]
+            profileDescription.text = profileSelected.description
+        }
     }
     
     
     // MARK: - Helper Functions
     
-
+    
     func processStudentNotes() {
-            /// Setup
+        /// Setup
         // let stringToProcess = "a soft ~#~Here fkjbf;;kjkfjvkjf;fjjfjfjf; I want to capture~#~ orange cat"
         let delimeter = "~#~"
-
+        
         /// Function Call
         do {
             let (extractedString, cleanString) = try HelperFunctions.getStringFrom(passedString: student.notes, using: delimeter)
@@ -180,7 +150,7 @@ class StudentProfileViewController: UIViewController {
             
             /// Process Cleaned String
             print(cleanString)
-
+            
             /// Process Extracted  String
             let strSplit2 = String(extractedString).split(separator: ";")
             // let strSplit3 = extractedString.split(separator: ";", maxSplits: 100, omittingEmptySubsequences: false)
@@ -190,31 +160,19 @@ class StudentProfileViewController: UIViewController {
                 profileForTheDayArray[idx] = (String(item))
                 print("`````loading \(String(item))")
             }
-
+            
             
             strSplit2.forEach { (item) in
                 print("--",item)
             }
-
+            
         } catch  {
             setAlready = false
             return
         }
-        
-        //let (extractedString, cleanString) = try! HelperFunctions.getStringFrom(passedString: student.notes, using: delimeter)
-
+         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension StudentProfileViewController: UITableViewDelegate, UITableViewDataSource {
@@ -234,14 +192,12 @@ extension StudentProfileViewController: UITableViewDelegate, UITableViewDataSour
         indexPathDictionary[profile.name] = indexPath
 
         return cell
-
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let profileSelected = profiles[indexPath.row]
         profileDescription.text = profileSelected.description
         let row = indexPath.row
-        print("888888888", row)
         profileForTheDayArray[segmentMovingFrom] = profiles[row].name
     }
     
