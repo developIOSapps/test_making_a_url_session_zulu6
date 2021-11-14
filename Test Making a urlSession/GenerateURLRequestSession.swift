d424583f728b500d8c37294b0da4ca6243a78d61
Steven Hertz <hertzsteven@icloud.com>
2021-11-14 18:38:22 -0500

    include generate teacher authentication

--- Test Making a urlSession/GenerateURLRequestSession.swift
+++ Test Making a urlSession/GenerateURLRequestSession.swift
@@ -13,45 +13,64 @@ enum URLValues {
     static let authorizationValue = "Basic NTM3MjI0NjA6RVBUTlpaVEdYV1U1VEo0Vk5RUDMyWDVZSEpSVjYyMkU="
     static let authorizationName = "Authorization"
 
-    static let xServerProtocolVersionValue = "3"
+    static let xServerProtocolVersionValueV3 = "3"
+    static let xServerProtocolVersionValueV2 = "2"
     static let xServerProtocolVersionName = "X-Server-Protocol-Version"
     
     static let cookieValue = "Hash=f59c9e4a0632aed5aa32c482301cfbc0; hash=78be3e9f9fb5aff8587c93c7a3b3b5f1"
     static let cookieName = "Cookie"
     
+    static let ContentTypeValue = "application/json; charset=utf-8"
+    static let ContentTypeName = "Content-Type"
+    
+
+    
     static let urlStringForListOfClasses = "https://api.zuludesk.com/classes"
     static let urlStringForClassInfo = "https://api.zuludesk.com/classes"
     static let urlStringForStudentPic = "https://api.zuludesk.com/classes"
+    static let urlStringForTeacherAuthenticate = "https://api.zuludesk.com/teacher/authenticate"
 
     
     case urlForListOfClasses
     case urlForClassInfo(UUISString: String)
     case urlForStudentPic(picUrlString: String)
+    case urlForTeacherAuthenticate
 
     
     func getUrlRequest(with urlString: String? = nil) -> URLRequest {
         
         let myUrl = self.urlforCase
-        
-        switch self {
-        case .urlForListOfClasses:
-            break
-        case .urlForClassInfo: break
-//            if let urlString = urlString {
-//                myUrl.appendPathComponent(urlString)
-//            }
-        case .urlForStudentPic: break
-//            guard let urlString = urlString, let urlForStudentPicture = URL(string: urlString) else {fatalError("Could not convert to url") }
-//            myUrl = urlForStudentPicture
-        }
-        
+                
         var request = URLRequest(url: myUrl)
         request.httpMethod = "GET"
         
         // Headers
         request.addValue(URLValues.authorizationValue, forHTTPHeaderField: URLValues.authorizationName)
-        request.addValue(URLValues.xServerProtocolVersionValue, forHTTPHeaderField: URLValues.xServerProtocolVersionName)
         request.addValue(URLValues.cookieValue, forHTTPHeaderField: URLValues.cookieName)
+        
+        
+        switch self {
+        case .urlForListOfClasses:  request.addValue(URLValues.xServerProtocolVersionValueV3, forHTTPHeaderField: URLValues.xServerProtocolVersionName)
+
+        case .urlForClassInfo:      request.addValue(URLValues.xServerProtocolVersionValueV3, forHTTPHeaderField: URLValues.xServerProtocolVersionName)
+
+        case .urlForStudentPic:     request.addValue(URLValues.xServerProtocolVersionValueV3, forHTTPHeaderField: URLValues.xServerProtocolVersionName)
+
+        case .urlForTeacherAuthenticate:
+            request.addValue(URLValues.xServerProtocolVersionValueV2, forHTTPHeaderField: URLValues.xServerProtocolVersionName)
+            request.addValue(URLValues.ContentTypeValue, forHTTPHeaderField: URLValues.ContentTypeName)
+            request.httpMethod = "Post"
+
+            // JSON Body
+
+            let bodyObject: [String : Any] = [
+                "company": "1049131",
+                "username": "morahchumie",
+                "password": "Chummie@864"
+            ]
+            request.httpBody = try! JSONSerialization.data(withJSONObject: bodyObject, options: [])
+        }
+        
         return request
     }
     
@@ -81,11 +100,17 @@ enum URLValues {
             if let  yUrl = URL(string: picUrlString) {
                 myUrl = yUrl
             }
+        
+        case .urlForTeacherAuthenticate:
+            if let  yUrl = URL(string: Self.urlStringForTeacherAuthenticate) {
+                myUrl = yUrl
+            }
+            
             
         }
         
         return myUrl
 
-    }    
+    }
 
 }
